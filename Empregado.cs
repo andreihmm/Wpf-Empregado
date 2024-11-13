0923dc68db8910ff0057ad3c182f120272c6c5fb
Andrei Alves Fracalossi <124471216+andreihmm@users.noreply.github.com>
2024-11-13 08:47:38 -0300

    Add files via upload

---
+++ Empregado.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1
+{
+    internal class Empregado
+    {
+
+        public int Matricula { get; set; } = 0;
+        public string CPF { get; set; }
+
+        public string Nome { get; set; }
+
+        public string Endereco { get; set; }
+
+        public Empregado() { }
+
+        public Empregado(int matricula, string cPF, string nome, string endereco)
+        {
+            Matricula = matricula;
+            CPF = cPF;
+            Nome = nome;
+            Endereco = endereco;
+        }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+
+        public void Salvar(SqlConnection conexao) 
+        {
+            Console.WriteLine("== Salvando empregado..... ==");
+            var Cmd = conexao.CreateCommand();
+            Cmd.CommandText = "INSERT INTO Empregado (Cpf, Nome, Endereco) VALUES (@cpf, @nome, @endereco)";
+
+            Cmd.Parameters.Add(new SqlParameter("cpf", CPF));
+            Cmd.Parameters.Add(new SqlParameter("nome", Nome));
+            Cmd.Parameters.Add(new SqlParameter("endereco", Endereco));
+
+            Cmd.ExecuteNonQuery();
+
+        }
+        public void Pesquisar() { }
+
+        public void Excluir() { }
+
+
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.Data.SqlClient;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        SqlConnection? conexao = null;
+
+        public MainWindow()
+        {
+
+            InitializeComponent();
+
+            string URL = "";
+            try
+            {
+                conexao = new(URL);
+                conexao.Open();
+                LabelStatus.Content = "Conexão OK";
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+
+        private bool VerificarCampos()
+        {
+            return TextBoxCpf.Text != "" && TextBoxNome.Text != "" && TextBoxEndereco.Text != "" && TextBoxMatricula.Text == "";
+        }
+
+        private void Limpar()
+        {
+            TextBoxCpf.Text = "";
+            TextBoxNome.Text = "";
+            TextBoxEndereco.Text = "";
+            TextBoxMatricula.Text = "";
+        }
+
+        private void BotaoSalvar_Click(object sender, RoutedEventArgs e)
+        {
+            Empregado emp = new()
+            {
+                CPF = TextBoxCpf.Text,
+                Nome = TextBoxNome.Text,
+                Endereco = TextBoxEndereco.Text,
+            };
+
+            if ((conexao != null) && VerificarCampos())
+            {
+                emp.Salvar(conexao);
+                LabelStatus.Content = "Salvo OK";
+                Limpar();
+            }
+            else
+            {
+                LabelStatus.Content = "Não salvo DesOK";
+
+            }
+        }
+
+        private void BotaoLimpar_Click(object sender, RoutedEventArgs e)
+        {
+            Limpar();
+        }
+    }
+}
